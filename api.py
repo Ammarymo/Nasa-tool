30179f8494d2019e48d36e05c8a5ddc6dac42b69
Ammar Y. Mohamed <115370824+Ammarymo@users.noreply.github.com>
2024-09-27 00:24:07 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,19 +1,15 @@
 import requests
-import json
 
-"""
-https://osdr.nasa.gov/osdr/data/osd/files/{OSD_STUDY_IDs}/?page={CURRENT_PAGE_NUMBER}&size={RESULTS_PER_PAGE}?all_files={ALL_FILES}
-"""
-
-#url = f"https://osdr.nasa.gov/bio/repo/search?q=mouse%20AND%20RNA-seq%20AND%20spaceflight&data_source=cgene"
+#My query ----- Not from repo
 url = f"https://osdr.nasa.gov/osdr/data/search?ffield=organism&fvalue=Mus%20musculus&ffield=Study%20Assay%20Technology%20Type&fvalue=RNA%20Sequencing"
+#Note ffield and the corresponding ffvalue (used for filtering)
 
-filters = {"api_key":"Aw9D8rwi4j2aRhFPrGNVKYEiqozSYUaDdRb45Miq"}
 
+response = requests.get(url).json() #for getting json
 
-response = requests.get(url).json()
-
+#looping through json and get the OSDs
 accession_ids = []
+
 for hit in response['hits']['hits']:
     accession = hit['_source'].get('Accession')
     if accession:
